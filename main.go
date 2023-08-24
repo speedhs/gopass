a8c770b5bb7847a05362c2c254233049bde0da78
speedhs <cocattackbased@gmail.com>
2023-08-24 13:37:27 +0530

    menu added

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 type data struct {
 	platform string
@@ -8,16 +11,63 @@ type data struct {
 	pass     string
 }
 
+func getUserChoice() int {
+	var choice int
+	fmt.Print("Enter your choice: ")
+	_, err := fmt.Scanf("%d", &choice)
+	if err != nil {
+		fmt.Println("Invalid input. Please enter a valid number.")
+		return -1
+	}
+	return choice
+}
+func prinLogo() {
+	fmt.Println(`
+   ____   _      _____  
+  / ___| | |    | ____| 
+ | |  _  | |    | |__   
+ | |_| | | |___ |  __|  
+  \____| |_____||_|
+	`)
+}
+func showOptions() {
+	fmt.Println("CLI Password Manager")
+	fmt.Println("1. Add Password")
+	fmt.Println("2. Retrieve Password")
+	fmt.Println("3. Update Password")
+	fmt.Println("4. Delete Password")
+	fmt.Println("5. Generate Password")
+	fmt.Println("6. Password Policy")
+	fmt.Println("7. Exit")
+}
+func mainMenu() {
+	prinLogo()
+	showOptions()
+}
 func addpass(obj data) {
 
 }
+
 func main() {
-	var x int
-	fmt.Scan(&x)
-	fmt.Println("hi" + fmt.Sprint(x))
-	switch x {
-	case 1: //Add data
-	case 2: //Retrieve data
-	case 3: //Generate strong password
+	mainMenu()
+	choice := getUserChoice()
+	switch choice {
+	case 1:
+		fmt.Println("Adding a new password...")
+	case 2:
+		fmt.Println("Retrieving a password...")
+	case 3:
+		fmt.Println("Updating a password...")
+	case 4:
+		fmt.Println("Deleting a password...")
+	case 5:
+		fmt.Println("Generating password...")
+	case 6:
+		fmt.Println("Password Policy")
+	case 7:
+		fmt.Println("Exiting the password manager.")
+		os.Exit(0)
+	default:
+		fmt.Println("Invalid choice. Please select a valid option.")
 	}
 }
